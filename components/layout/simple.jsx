7a45a725f284f3c5a926e404013ab92784d7f861
Alucard <minxter.yt@gmail.com>
2022-02-24 05:20:49 +0000

    Update simple.jsx

--- components/layout/simple.jsx
+++ components/layout/simple.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 const variants = {
     hidden: { opacity: 0, x: 0, y: 0 },
     enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0.2, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: 0 },
 }
 
 export default function SimpleLayout(props) {
